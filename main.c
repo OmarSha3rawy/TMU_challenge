3008e9c9ce4dd2ce333fa097a68b25f2369225df
OmarSha3rawy <47915917+OmarSha3rawy@users.noreply.github.com>
2019-10-13 14:10:55 +0200

    Tmu (#1)
    * initial veriosn with designs
    
    * working version
    
    * ready to be merged

---
+++ main.c
@@ -0,0 +1,66 @@
+/*
+ * TMU.c
+ *
+ * Created: 10/12/2019 7:53:19 PM
+ * Author : omara
+ */ 
+
+#include "REG_Lib.h"
+#include "DIO.h"
+#include "tmu.h"
+#define F_CPU 1000000UL 
+
+void processor_sleep();
+void togg_led();
+void togg_led2();
+
+
+
+int main(void)
+{
+	//DDRC = 0xff; //for tesring
+	DIO_init();
+	TMU_init(TIMER0, 1);
+	TMU_start(togg_led,PERIODIC, 2000 );
+	//TMU_start(togg_led2,PERIODIC, 5000 );
+
+    while (1) 
+    {
+		TMU_dispatcher();
+		
+		processor_sleep();
+    }
+}
+
+
+void togg_led()
+{
+	uint8 result;
+	DIO_read(led0, &result);
+	if(result)
+	{
+		DIO_write(led0, LOW);
+	}
+	else
+	{
+		DIO_write(led0, HIGH);
+	}
+}
+void togg_led2()
+{
+	uint8 result;
+	DIO_read(led1, &result);
+	if(result)
+	{
+		DIO_write(led1, LOW);
+	}
+	else
+	{
+		DIO_write(led1, HIGH);
+	}
+}
+
+void processor_sleep()
+{
+	// stub to act as a sleep function
+}
---
+++ tmu.c
@@ -0,0 +1,116 @@
+/*
+ * tmu.c
+ *
+ * Created: 10/12/2019 7:57:15 PM
+ *  Author: omara
+ */ 
+
+#include "tmu.h"
+
+buff buffer [MAXNUMBEROfTASKS];
+
+flag_state tick_flag = DISABLED;
+uint32 ov_counter = 0;
+uint8 ov_counter_assistant = 0;
+
+uint8 res;
+
+void overflow_count()
+{
+	ov_counter_assistant++;
+	if( (ov_counter_assistant) == 4 )
+	{
+		ov_counter++;
+		ov_counter_assistant = 0;
+		tick_flag = ACTIVE;
+	}
+}
+
+uint8 TMU_init(uint8 id, uint8 resolution)
+{
+	res = resolution;
+	switch(id)
+	{
+		case TIMER1:
+			TIMER_cnfg_arr[0].TIMER_ID = TIMER1;
+			TIMER1_CMPA_setCallBack(overflow_count);
+		break;
+		
+		case TIMER2:
+			TIMER_cnfg_arr[0].TIMER_ID = TIMER2;
+			TIMER2_CMP_setCallBack(overflow_count);
+		break;
+		
+		default:
+			TIMER_cnfg_arr[0].TIMER_ID = TIMER0;
+			TIMER0_CMP_setCallBack(overflow_count);
+		break;
+	}
+	TIMER_init();
+	return OK;
+}
+
+uint8 TMU_start(void (*application)(void), periodicity per, uint32 freq)
+{
+	uint8 i;
+	for(i = 0; i < MAXNUMBEROfTASKS; i++)
+	{
+		if(buffer[i].app_ptr == NULL)
+		{
+			buffer[i].app_ptr = application;
+			buffer[i].per = per;
+			buffer[i].freq = freq;
+			buffer[i].state = RUNNING ;
+			buffer[i].start = ov_counter;
+			return OK;
+		}
+	}
+	return NOK;
+}
+
+uint8 TMU_stopTimer(void (*application)(void))
+{
+	uint8 i;
+	for(i = 0; i < MAXNUMBEROfTASKS; i++)
+	{
+		if(buffer[i].app_ptr == application)
+		{
+			buffer[i].app_ptr = NULL;	
+			buffer[i].state = FINISHED;
+			return OK;
+		}
+	}
+	return NOK;
+}
+
+uint8 TMU_dispatcher()
+{
+	uint8 i;
+	if (tick_flag == ACTIVE)
+	{
+		for(i = 0; i < MAXNUMBEROfTASKS; i++)
+		{
+			//if( ( ((ov_counter - buffer[i].start) % buffer[i].freq == 0) || ((buffer[i].start - ov_counter) % buffer[i].freq == 0) ) && (buffer[i].state == RUNNING) )
+			//if( ( ((ov_counter - buffer[i].start) % buffer[i].freq == 0) ) && (buffer[i].state == RUNNING) )
+			if( ov_counter % buffer[i].freq == 0  && (buffer[i].state == RUNNING) )
+
+
+			{
+				//buffer[i].start = ov_counter;
+				if(buffer[i].per == ONE_SHOT)
+				{
+					(buffer[i].app_ptr)();
+					buffer[i].app_ptr = NULL;
+					buffer[i].state = FINISHED;					
+				}
+				else
+				{
+					(buffer[i].app_ptr)();
+				}
+			}
+		}
+		tick_flag = DISABLED;
+	}
+	return OK;
+}
+
